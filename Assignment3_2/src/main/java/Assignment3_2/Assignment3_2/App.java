06bea5e3bb3f23f25418e5c0598875fd46eeeed6
michael <michael@156.59.131.0>
2019-10-09 17:06:24 +1300

    commit

---
+++ Assignment3_2/src/main/java/Assignment3_2/Assignment3_2/App.java
@@ -0,0 +1,13 @@
+package Assignment3_2.Assignment3_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
